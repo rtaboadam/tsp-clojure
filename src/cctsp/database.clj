8947c072a5844d289f5bd8191034238820f08845
Ricardo Taboada <tabo.odricar@gmail.com>
2017-09-01 16:57:55 -0500

    [ADDED] more function about persisntence

--- src/cctsp/database.clj
+++ src/cctsp/database.clj
@@ -10,6 +10,13 @@
     :subname "resources/world.db"
   })
 
+(defn cities_length
+  "Devuelve la cantidad de ciudad que hay en la base de datos
+    Parametros
+      :db La base de datos"
+  [db]
+  (-> (sql/query db "SELECT * FROM cities") count))
+
 (defn get_neighbors
   "Función que regresa los vecinos de una ciudad i.e ciudades adyacentes
     Parametros
@@ -17,5 +24,19 @@
       :db la base de datos
   " 
   [city db]
-  (sql/query db ["SELECT id_city_2 ,distance FROM connections WHERE id_city_1 = ?", (str city)]))
+  (vec (sql/query db ["SELECT id_city_2 ,distance FROM connections WHERE id_city_1 = ?", (str city)])))
 
+(defn serializable?
+  "Función que dice si algo es serializable
+    Parametros
+      :v el objeto a evaluar"
+  [v]
+  (instance? java.io.Serializable v))
+
+(defn make_matrix 
+  "Función que crea una matrix de r x c
+    Parametros:
+      :r Renglones
+      :c Columnads"
+  [r c]
+  (vec (repeat c (vec (repeat r 0)))))
