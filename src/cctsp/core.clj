8716176dcc3580ee7c253882393fe0c0e545263e
Ricardo <tabo.odricar@gmail.com>
2017-09-23 16:12:00 -0500

    [ADDED] Some functions about cities

--- src/cctsp/core.clj
+++ src/cctsp/core.clj
@@ -4,7 +4,36 @@
   (:refer-clojure :exclude [rand rand-int rand-nth])
   (:gen-class))
 
+
+(declare cost-function)
+(declare gen-city)
+(declare distance-memo)
+(declare distance)
+
+(defprotocol Point
+	(distance-to [this city])
+	(to-string [this]))
+
+(defprotocol PointCollection
+	(total-distance [this]))
+
+(defrecord City [name latitude longitude]
+	Point
+	(distance-to [this city] (distance-memo this city))
+	(to-string [this] (str (:name this) ":(" (:latitude this) "," (:longitude this))))
+
+(defrecord Tour [cities]
+  PointCollection
+  (total-distance [this]
+  	(let [cities (:cities this)] 
+  		(loop [curr cities distances '()]
+  			(if (empty? (rest curr))
+  				(reduce + distances)
+  				(recur (rest curr) (conj distances (distance-to (first curr) (second curr))))
+  				)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
